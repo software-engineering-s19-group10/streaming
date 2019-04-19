5013b15295b73dfa2c314def66381b074d6588c0
mnadev <mohammednadeem902@gmail.com>
2019-04-19 18:30:43 -0400

    added api endpoints for locks

--- server/api_handler.py
+++ server/api_handler.py
@@ -22,9 +22,24 @@ def get_auth_token(username="smart-lock-admin", password="copyandpaste"):
 
 # Add endpoint and handler for the embedding
 
-#
 
-#get_auth_token()
+get_auth_token()
+
+
+
+def get_user():
+    route = 'api/owners/'
+    params = {"Authorization": AUTH_TOKEN}
+    res = req.get(BASE_URL + route, params).json()
+
+    #id = res[0]["id"]
+    #route = 'api/owners/' + str(id)
+    #params = {"Authorization": AUTH_TOKEN}
+    #res = req.get(BASE_URL + route, params).json()
+    print (res)
+    return res
+
+user_info = get_user()[0]
 
 def get_ip():
     s = sock.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -40,17 +55,37 @@ get_ip()
 def get_public_ip():
     return req.get("https://api.ipify.org/?format=json").json()["ip"]
 
-
-def post_ip_address(lock_owner, address, ip_address):
+def get_lock(lock_owner=user_info["id"]):
     route = 'api/locks/'
-    params = {"Authorization": AUTH_TOKEN, "lock_owner": lock_owner, "address": address, "ip_address": address}
+    params = {"Authorization": AUTH_TOKEN, "lock_owner": lock_owner}
+    res = req.post(BASE_URL + route, params).text
+    print(res)
+    return
+
+
+lock_info = get_lock()
+
+def create_lock(lock_owner=user_info["id"], address="54 Brett Rd., Piscataway, NJ", ip_address=get_ip()):
+    route = 'api/locks/'
+    params = {"Authorization": AUTH_TOKEN, "lock_owner": lock_owner, "address": address, "ip_address": ip_address}
+    req.post(BASE_URL + route, params)
+    return
+
+
+def post_ip_address(lock_owner=user_info["id"], address="54 Brett Rd., Piscataway, NJ", ip_address=get_ip()):
+    route = 'api/locks/'
+    params = {"Authorization": AUTH_TOKEN, "lock_owner": lock_owner, "address": address, "ip_address": ip_address}
     req.patch(BASE_URL + route, params)
     return
+
+post_ip_address()
+
+lock_info = get_lock()
 
 
 def post_srn(lat, longitude, date_str, lock_id):
     route = 'api/srn/'
-    params = {"Authorization": AUTH_TOKEN, "latitude": lat, "longitude": longitude, "stranger_report_time":date_str, "lock":lock_id}
+    params = {"Authorization": AUTH_TOKEN, "latitude": lat, "longitude": longitude, "stranger_report_time": date_str, "lock": lock_id}
 
     req.post(BASE_URL + route, params)
     return
