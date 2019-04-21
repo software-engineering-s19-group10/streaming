e7f087a48aac9251c3e32bda0172c6e8cea77fa3
mnadev <mohammednadeem902@gmail.com>
2019-04-20 22:36:44 -0400

    fixed issues with segfaults in the server

--- server/pi_vid_sender.py
+++ server/pi_vid_sender.py
@@ -3,28 +3,28 @@ from websocket_server import WebsocketServer
 from time import sleep
 from PIL import Image
 import api_handler
-from video_api import set_up_cap, get_frame
-
-
-
-global vid
-
-vid = set_up_cap()
 
 
 def start_sending(client, server):
+    # Create a VideoSender and start sending video
+    vid = cv2.VideoCapture(0)
+
+    if not vid.isOpened():
+        print("Could not open video feed.")
+        exit(-1)
+
+    print("Connected.")
 
     print("Sending Data Now")
-    
+
     # Keep sending forever until connection terminated
     while True:
 
-        frame = get_frame(vid)
+        ret, frame = vid.read()
 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        if not frame is None:
-            
             # Convert to bytes
             pil_im = Image.fromarray(frame)
             b = io.BytesIO()
@@ -40,6 +40,7 @@ def start_sending(client, server):
         else:
             continue
 
+
 def create_server():
     PORT = 8080
     server = WebsocketServer(PORT)
@@ -47,34 +48,5 @@ def create_server():
     server.run_forever()
 
 
-def print_im():
-    print("Sending Data Now")
-
-    # Keep sending forever until connection terminated
-    while True:
-
-        frame = get_frame(vid)
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        if not frame is None:
-
-            # Convert to bytes
-            pil_im = Image.fromarray(frame)
-            b = io.BytesIO()
-
-            # convert to jpeg encoding
-            pil_im.save(b, 'jpeg')
-            im_bytes = b.getvalue()
-
-            print(binascii.b2a_base64(im_bytes))
-            break
-
-
-        else:
-            continue
-
-# TODO: Program will take one command line argument which will be the auth key
-if __name__=="__main__":
-    # print_im()
+if __name__ == "__main__":
     create_server()
